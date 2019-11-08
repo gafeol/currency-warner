1002f243cc1bd2f9157b5e841b7c6af5b154e946
gafeol <gabfeol@gmail.com>
2019-11-08 19:53:45 +0100

    Material UI + Abstract currency

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Value from './Value';
+import { TextField, MenuItem, Box } from '@material-ui/core';
+
 
 function App() {
+  const [origCurrency, setOrigCurrency] = useState("USD");
+  const [destCurrency, setDestCurrency] = useState("BRL");
+
+  const changeOrig = (event) => {
+    setOrigCurrency(event.target.value);
+  }
+
+  const changeDest = (event) => {
+    setDestCurrency(event.target.value);
+  }
+
   return (
     <div className="App">
-      <Value />
+      <Box display="flex" justifyContent="space-around" m={5}>
+        <TextField style={{minWidth: 150}}
+          select
+          value={origCurrency}
+          onChange={changeOrig}
+          label="Origin currency"
+        >
+          {currencies.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <TextField style={{minWidth: 150}}
+          select
+          value={destCurrency}
+          onChange={changeDest}
+          label="Destination currency"
+        >
+          {currencies.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Box>
+
+      <Value mt={2} origin={origCurrency} dest={destCurrency}/>
     </div>
   );
 }
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+  {
+    value: 'BRL',
+    label: 'R$'
+  }
+];
 
 export default App;
--- client/src/Value.js
+++ client/src/Value.js
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-async function getQuotation(){
-    const url = "https://api.exchangeratesapi.io/latest?base=CHF&symbols=BRL"
-    const response = await axios.get(url)
+const getQuotation = async (orig, dest) => {
+    const url = `https://api.exchangeratesapi.io/latest?base=${orig}&symbols=${dest}`;
+    const response = await axios.get(url);
     console.log("RESPONSE ", JSON.stringify(response));
-    return response.data.rates.BRL;
+    return response.data.rates[dest];
 }
 
-const Value = () => {
+const Value = ({origin, dest}) => {
     const [loading, setLoading] = useState(true)
-    const [value, setValue] = useState(1);
-    getQuotation().then((val) => {
+    const [value, setValue] = useState();
+
+    getQuotation(origin, dest).then((val) => {
         setValue(val);
         setLoading(false)
     })
+
     return (
         <div>
             <h1> {loading ? 
-                "Loading..." : 
-                ("1 CHF = " + value + " BRL")
+                <CircularProgress/> : 
+                ("1 " + origin + " = " + value + " " + dest)
                 }
             </h1>
         </div>
