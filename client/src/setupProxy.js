125770a0712eb9c1190122b0d7ce87dd231b589d
gafeol <gabfeol@gmail.com>
2020-01-09 17:48:01 +0100

    change backend port

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,6 @@
 const proxy = require('http-proxy-middleware')
 
+const PORT = process.env.PORT || 5000;
 module.exports = function(app) {
-    app.use(proxy('/api/*', { target: `http://[::1]:5000` }))
+    app.use(proxy('/api/*', { target: `http://[::1]:${PORT}` }))
 }
