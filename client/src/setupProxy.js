63c8b71e99e6f8e19505051470d6ab7be0001d78
gafeol <gabfeol@gmail.com>
2020-01-09 17:38:31 +0100

    fixes proxy problems

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,5 @@
 const proxy = require('http-proxy-middleware')
 
-const PORT = process.env.PORT || 5000;
 module.exports = function(app) {
-    app.use(proxy('/api/*', { target: `http://[::1]:${PORT}` }))
+    app.use(proxy('/api/*', { target: `http://[::1]:5000` }))
 }
